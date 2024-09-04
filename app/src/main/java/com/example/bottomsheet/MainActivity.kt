f1882a5ac645629f3750aeed132a9a0d13e64183
AnilKumarSingh9856 <anil.21scse1010654@galgotiasuniversity.edu.in>
2024-09-05 00:59:52 +0530

    commit

---
+++ app/src/main/java/com/example/bottomsheet/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.example.bottomsheet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.bottomsheet.ui.theme.BottomSheetTheme
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BottomSheetTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val sheetState = rememberModalBottomSheetState()
+                    var isSheetOpen by rememberSaveable {
+                        mutableStateOf(false)
+                    }
+                    val scaffoldState = rememberBottomSheetScaffoldState()
+                    val scope = rememberCoroutineScope()
+
+
+                    BottomSheetScaffold(
+                        scaffoldState = scaffoldState,
+                        sheetContent = {
+                            Image(
+                                painter = painterResource(id = R.drawable.kermit),
+                                contentDescription = null
+                            )
+                        },
+                        sheetPeekHeight = 0.dp
+                    ) {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Button(onClick = {
+                                scope.launch {
+                                    scaffoldState.bottomSheetState.expand()
+                                }
+                            }) {
+                                Text(text = "Open sheet")
+                            }
+                        }
+                    }
+
+//                    if(isSheetOpen) {
+//                        ModalBottomSheet(
+//                            sheetState = sheetState,
+//                            onDismissRequest = {
+//                                isSheetOpen = false
+//                            },
+//                        ) {
+//                            Image(
+//                                painter = painterResource(id = R.drawable.kermit),
+//                                contentDescription = null
+//                            )
+//                        }
+//                    }
+                }
+            }
+        }
+    }
+}
